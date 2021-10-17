3702c279d91fc292c14693d37a8c400c674b4fd5
Balaji Sukumaran <balajisukumaran96@gmail.com>
2021-10-17 17:28:16 +0530

    Rough Skeleton

---
+++ Tinder.API/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tinder.API.Data;
+using Tinder.API.Models;
+
+namespace Tinder.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id) {
+            var value = await _context.Values.FirstOrDefaultAsync<Value>(x=>x.Id==id);
+            return Ok(value);
+        }
+    }
+}
--- Tinder.API/Startup.cs
+++ Tinder.API/Startup.cs
@@ -28,7 +28,7 @@ namespace Tinder.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
@@ -48,7 +48,7 @@ namespace Tinder.API
             }
 
             // app.UseHttpsRedirection();
-
+            app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseRouting();
 
             app.UseAuthorization();
